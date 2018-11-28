59f59b6af5adf965fcbad25a6443c0b195defe29
godn0117 <godn0117@gmail.com>
2018-11-28 12:02:46 +0900

    Student만들고 mbox출력

---
+++ GitTest1/student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitTest1
+{
+    public partial class student : Form
+    {
+        public student()
+        {
+            InitializeComponent();
+        }
+
+        private void student_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Student");
+        }
+    }
+}
