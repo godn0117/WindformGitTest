6146a30ee69329936f8245f729cc50edbf4ddff8
wldnd00468 <wldnd00468@gmail.com>
2018-11-28 17:03:30 +0900

    godgordon

---
+++ GitTest1/BtnGoodee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitTest1
+{
+    public partial class BtnGoodee : Form
+    {
+        public BtnGoodee()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGoodee_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("그의 이름은 ");
+        }
+    }
+}
