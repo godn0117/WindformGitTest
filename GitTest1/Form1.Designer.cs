9aa9b4df6c831acedd7f57593485da883b91fd3a
indichi1396 <indichi1396@gmail.com>
2018-11-28 17:05:44 +0900

    who is captain

--- GitTest1/Form1.Designer.cs
+++ GitTest1/Form1.Designer.cs
@@ -104,17 +104,13 @@
             this.label2.Size = new System.Drawing.Size(38, 12);
             this.label2.TabIndex = 6;
             this.label2.Text = "label2";
-            // button4
-            // 
-
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(979, 456);
+            this.ClientSize = new System.Drawing.Size(1019, 525);
             this.Controls.Add(this.label2);
-
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label1);
---
+++ GitTest1/frmGoldenBell.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitTest1
+{
+    public partial class frmGoldenBell : Form
+    {
+        public frmGoldenBell()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Captain is Golden!!!!!!");
+        }
+    }
+}
